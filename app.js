799aa6f70735b79ffd8f11e32aaef2008811478a
devinsg <duyanh2005@gmail.com>
2020-09-28 20:34:20 +0700

    - clean up

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
@@ -16,11 +15,10 @@ app.use(bodyParser.json());
 // Set up cookies and sessions to save tokens
 app.use(cookieParser());
 app.use(session({ 
-    secret: '0dc529ba-5051-4cd6-8b67-c9a901bb8bdf',
-    resave: false,
-    saveUninitialized: false 
-  })
-);
+  secret: '0dc529ba-5051-4cd6-8b67-c9a901bb8bdf',
+  resave: false,
+  saveUninitialized: false 
+}));
   
 // Home page
 app.get('/', function(req, res) {
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    endpoint: "<Your Azure Cosmos account URI>",
-    key: "<Your Azure Cosmos account key>",
+    endpoint: "https://azcomos.documents.azure.com:443/",
+    key: "mtGhFrHf41R8icrVGBWCKiERYLQbM1EOIALEO8c6jgHFbYvex4r0H1fdAXoNB3NSD3lg1Dkvl09VQqPrkZWsFA==",
     databaseId: "Tasks",
     containerId: "Items",
     partitionKey: { kind: "Hash", paths: ["/category"] }
